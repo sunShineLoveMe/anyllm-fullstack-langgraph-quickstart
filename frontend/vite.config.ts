cd32c478f28c69b47e52ad7c09eb8b4b5883489b
sunShineLoveMe <juneandrao@outlook.com>
2025-06-11 18:12:14 +0800

    修改nginx

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
-  base: "/app/",
+  base: "/cloudthink/",
   server: {
     host: '0.0.0.0',
     port: 5173,
